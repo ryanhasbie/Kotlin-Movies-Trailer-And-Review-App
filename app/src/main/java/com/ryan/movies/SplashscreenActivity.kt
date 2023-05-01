ffb4b72309405deb447c84f1557298742313b853
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-05-01 22:55:22 +0700

    Setup project

---
+++ app/src/main/java/com/ryan/movies/SplashscreenActivity.kt
@@ -0,0 +1,16 @@
+package com.ryan.movies
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2000)
+    }
+}
