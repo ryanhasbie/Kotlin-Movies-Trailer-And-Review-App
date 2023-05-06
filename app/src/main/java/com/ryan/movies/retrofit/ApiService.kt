8b7c12f8cd7cde8d5826255895dd81d7e6bb66c6
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-05-06 10:16:04 +0700

    interceptor

--- app/src/main/java/com/ryan/movies/retrofit/ApiService.kt
+++ app/src/main/java/com/ryan/movies/retrofit/ApiService.kt
@@ -1,5 +1,8 @@
 package com.ryan.movies.retrofit
 
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,6 +11,11 @@ const val BASE_URL = "https://api.themoviedb.org/3/movie/"
 class ApiService {
     val endpoint: ApiEndpoint
         get() {
+
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
